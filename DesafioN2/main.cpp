38af055d714a8328479cbdce4fe070dd51ed9444
didier <didie@gmail.com>
2026-04-24 22:08:38 -0500

    Actualizacion del menu y implementacion de nuevas funciones en torneo.h

--- DesafioN2/main.cpp
+++ DesafioN2/main.cpp
@@ -3,28 +3,62 @@
 
 using namespace std;
 int main() {
-    DesafioN2Libreria torneo;
+    Torneo torneo;
     int opcion = 0;
+
     do {
-        cout << "\n UdeAWorldCup " << endl;
+        cout << "========== UdeAWorldCup ==========" << endl;
         cout << "1. Cargar equipos " << endl;
-        cout << "2. Mostrar equipos cargados" << endl;
-        cout << "3. Sortear grupos" << endl;
-        cout << "4. Mostrar grupos" << endl;
+        cout << "2. Crear jugadores artificiales" << endl;
+        cout << "3. Mostrar equipos cargados" << endl;
+        cout << "4. Formar bombos" << endl;
+        cout << "5. Sortear grupos" << endl;
+        cout << "6. Mostrar grupos" << endl;
+        cout << "7. Generar partidos de grupos" << endl;
+        cout << "8. Simular fase de grupos" << endl;
+        cout << "9. Mostrar tablas de grupos" << endl;
+        cout << "10. Clasificar a dieciseisavos" << endl;
+        cout << "11. Simular fases finales" << endl;
+        cout << "12. Mostrar estadisticas finales" << endl;
         cout << "0. Salir" << endl;
         cout << "Seleccione una opcion: ";
         cin >> opcion;
         switch(opcion) {
         case 1:
+            torneo.cargarEquiposDesdeCSV("selecciones_clasificadas_mundial.csv");
             break;
         case 2:
-            torneo.mostrarEquipos();
+            torneo.crearJugadoresDeTodosLosEquipos();
             break;
         case 3:
-            torneo.sortearGrupos();
+            torneo.mostrarEquipos();
             break;
         case 4:
+            torneo.formarBombos();
+            break;
+        case 5:
+            torneo.sortearGrupos();
+            break;
+        case 6:
             torneo.mostrarGrupos();
+            break;
+        case 7:
+            torneo.generarPartidosDeGrupos();
+            break;
+        case 8:
+            torneo.simularFaseDeGrupos();
+            break;
+        case 9:
+            torneo.mostrarTablaDeGrupos();
+            break;
+        case 10:
+            torneo.clasificarADieciseisavos();
+            break;
+        case 11:
+            torneo.simularFasesFinales();
+            break;
+        case 12:
+            torneo.mostrarEstadisticasFinales();
             break;
         case 0:
             cout << "Saliendo..." << endl;
--- DesafioN2/torneo.h
+++ DesafioN2/torneo.h
@@ -4,16 +4,51 @@
 #include "grupo.h"
 
 class Torneo {
-
 private:
     Equipo* equipos;
     int cantidadEquipos;
+
     Grupo* grupos;
     int cantidadGrupos;
+
     Equipo** bombos[4];
     int cantidadesBombos[4];
 
+    Partido* partidosGrupos;
+    int cantidadPartidosGrupos;
+
+    Equipo** clasificadosR16;
+    int cantidadClasificadosR16;
+
+    Partido* partidosR16;
+    int cantidadPartidosR16;
+
+    Partido* partidosR8;
+    int cantidadPartidosR8;
+
+    Partido* partidosQF;
+    int cantidadPartidosQF;
+
+    Partido* partidosSF;
+    int cantidadPartidosSF;
+
+    Partido* partidosFinales;
+    int cantidadPartidosFinales;
+
+    Equipo** top4;
+
+    MedidorRecursos medidor;
+    bool equipoYaJuegaEseDia(Equipo* equipo, const Fecha& fecha, int hastaIndice) const;
+    bool equipoRompeReglaTresDias(Equipo* equipo, const Fecha& fecha, int hastaIndice) const;
+    void reiniciarDatosDeGrupo();
+    void ordenarTerceros(Equipo** terceros, int cantidad);
+    void ordenarEquiposPorGrupo(Equipo** lista, int cantidad);
+    Equipo* ganadorDePartido(const Partido& partido) const;
+    Equipo* perdedorDePartido(const Partido& partido) const;
+    void configurarPartidoEliminacion(Partido& partido, Equipo* a, Equipo* b,const char* etapa);
+
 public:
+
     Torneo();
     ~Torneo();
     void cargarEquiposDesdeCSV(const char* nombreArchivo);
@@ -24,8 +59,16 @@ public:
     void mostrarBombos() const;
     void sortearGrupos();
     void mostrarGrupos() const;
+    void generarPartidosDeGrupos();
+    void simularFaseDeGrupos();
+    void mostrarTablaDeGrupos();
+    void clasificarADieciseisavos();
+    void simularFasesFinales();
+    void mostrarEstadisticasFinales();
+    void mostrarRecursosBasicos(const char* funcionalidad);
     int getCantidadEquipos() const;
     Equipo* getEquipo(int i) const;
+
 };
 
 #endif
